4d0d4480b81be5b228b8a221653d9a3beb287783
khaldoonahmad <khaldoon_ahmad@hotmail.com>
2017-06-29 13:54:19 -0700

    hkdoon

--- customer/models.py
+++ customer/models.py
@@ -81,6 +81,12 @@ class Customer(models.Model):
 class CustomerCategory(models.Model):
     customer_code = models.ForeignKey(Customer)
     category_code = models.ForeignKey(Category)
+	customer_id = models.AutoField(primary_key=True)
+	customer_name = models.TextField(max_length=50)
+	customer_address = models.TextField(max_length=50)
+	credit_limit = MoneyField(max_digits=10, decimal_places=2, default_currency='USD')
+	payment_term = models.TextField(max_length=50, choices=payment_terms, default='EOM')
+	#price_list_code = models.ForeignKey(PriceList, on_delete=models.CASCADE)
 
     def __str__(self):
         return 'Category'

956468cc2c431cfefd7f052bb4449648d941d588
khaldoonahmad <khaldoon_ahmad@hotmail.com>
2017-06-29 13:54:19 -0700

    Khldoon first commit

--- customer/models.py
+++ customer/models.py
@@ -14,6 +14,7 @@ class Customer(models.Model):
 
 	customer_id = models.AutoField(primary_key=True)
 	customer_name = models.TextField(max_length=50)
+	customer_address = models.TextField(max_length=50)
 	credit_limit = MoneyField(max_digits=10, decimal_places=2, default_currency='USD')
 	payment_term = models.TextField(max_length=50, choices=payment_terms, default='EOM')
 	#price_list_code = models.ForeignKey(PriceList, on_delete=models.CASCADE)

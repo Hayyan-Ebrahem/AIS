ae35cf77034515d1f97c7427f7b05ba4e9935b4c
hayyan <hayyan-ebrahem@hotmail.com>
2017-06-30 11:46:09 +0200

    ef

--- product/models.py
+++ product/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Category(models.Model):
 	category_id = models.AutoField(primary_key=True)
-	name = models.CharField(max_length=22)
+	name = models.CharField(max_length=12)
 	description = models.TextField()
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)

2d5c459e347cac2f287ae8c610be2cf3af0f7458
hayyan <hayyan-ebrahem@hotmail.com>
2017-07-08 13:30:23 +0200

    price list status

--- product/models.py
+++ product/models.py
@@ -51,19 +51,12 @@ class ProductCategory(models.Model):
 
 class PriceList(models.Model):
 
-	status = (
-		('WIP','Payment in advance'),
-		('EOM','End of the month'),
-		('CWO','Cash with order'),
-	)
-
-
 	price_list_code = models.AutoField(primary_key=True)
 	description = models.TextField(max_length=100)
 	created_at = models.DateTimeField(auto_now_add=True)
 	validate_till = models.DateField()
 	blocked_till = models.DateField()
-	status = models.CharField(max_length=10, choices=status, default='WIP')
+	status = models.BooleanField(default=True)
 
 	def __str__(self):
 		return str(self.price_list_code)

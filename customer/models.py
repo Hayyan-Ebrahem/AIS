04c7c4eacc00803b91b4271f885d10198a9dca00
khaldoonahmad <khaldoon_ahmad@hotmail.com>
2017-07-08 14:20:16 -0700

    first edition

--- customer/models.py
+++ customer/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from djmoney.models.fields import MoneyField
 from django.conf import settings
+from django.models.fields import PositiveSmallIntigerFeild
 #from product.models import PriceList
 
 
@@ -12,12 +13,14 @@ class Customer(models.Model):
 		('CWO','Ch with order'),
 	)
 
-	customer_id = models.AutoField(primary_key=True)
-
-	customer_name = models.TextField(max_length=31)
-
-	#customer_address = models.TextField(max_length=50)
+	id = models.AutoField(primary_key=True)
+	name = models.TextField(max_length=30)
+	address = models.TextField(max_length=50)
+	phone_no = models.TextField(max_length=10)
+	email = models.TextField(max_length=30)
+	category_id = models.PositiveSmallIntigerFeild() 
 	credit_limit = MoneyField(max_digits=10, decimal_places=2, default_currency='USD')
+	credit_periosd = models.
 	payment_term = models.TextField(max_length=50, choices=payment_terms, default='EOM')
 	#price_list_code = models.ForeignKey(PriceList, on_delete=models.CASCADE)
 

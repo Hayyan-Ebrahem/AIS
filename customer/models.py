1c64c5d0e556c4901b48f9159888205ef4551013
khaldoonahmad <khaldoon_ahmad@hotmail.com>
2017-07-08 22:56:18 -0700

    from khldoon-code

--- customer/models.py
+++ customer/models.py
@@ -40,7 +40,48 @@ class Customer(models.Model):
     second_period = datetime.timedelta(days=60)
     third_period = datetime.timedelta(days=90)
 
+<<<<<<< HEAD
     duration_choices = ((first_period, '30'), (second_period, '60'), (third_period, '90'))
+=======
+	payment_terms = (
+		('PIA','Payment in advance'),
+		('EOM','End of the month'),
+		('CWO','Chash with order'),
+	)
+
+	shipping_terms = (
+		('CIF', 'Cost, Insurance and Freight'), 
+		('COP','Customs of the Port'), 
+		('DWT','Deadweight Tonnage'),
+	)
+
+	code = models.TextField(primary_key=True, max_length=30)
+	name = models.TextField(max_length=30, unique=True)
+	address = models.TextField(max_length=50)
+	phone_no = models.TextField(max_length=30, unique=True)
+	email = models.TextField(max_length=30, unique=True)
+	category_code = models.ForeignKey(CustomerCategory, on_delete=models.CASCADE)
+	#chart_of_account = models.TextField(max_length=30)
+	credit_limit = MoneyField(max_digits=10, decimal_places=2, default_currency='USD')
+	credit_period = models.DurationField()
+	payment_term = models.CharField(max_length=50, choices=payment_terms, default='EOM')
+	shipping_term = models.CharField(max_length=50, choices=shipping_terms, default='CIF')
+	ref_document_no = models.TextField(max_length=10)
+	ref_document_date = models.DateField()
+	user_id = models.TextField(max_length=15)
+	#should be related to user table will be done later
+	created_at = models.DateField(auto_now_add=True)
+	sales_man_id = models.TextField(max_length=15)
+	#should be related to user table will be done later
+	status = models.BooleanField()
+	#if 0 the status will appear Blocked, if 1 the status will appear active
+	active_till_date = models.DateField()
+	blocked_till_date = models.DateField()
+	#price_list_code = models.ForeignKey(PriceList, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.name
+>>>>>>> 516e995... Change credit_period data type to durationfield instead of datefield
 
     first_choice = 'day'
     second_choice = 'month'

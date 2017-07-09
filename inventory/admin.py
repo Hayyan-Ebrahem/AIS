810a0335273790a3b18ecca0c26554658aa0f5c2
khaldoonahmad <khaldoon_ahmad@hotmail.com>
2017-07-08 22:06:26 -0700

    Corrections to run admin site

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from inventoy.models import Warehouse, WarehouseCategory
+from inventory.models import Warehouse, WarehouseCategory
 # Register your models here.
 
 admin.site.register(Warehouse)

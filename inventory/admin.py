1ef5e55f14d01bb5ca11843362baf26c618549c8
khaldoonahmad <khaldoon_ahmad@hotmail.com>
2017-07-08 18:40:35 -0700

    Add Warehouse and Wh.Category to the admin site

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from inventoy.models import Warehouse, WarehouseCategory
 # Register your models here.
+
+admin.site.register(Warehouse)
+admin.site.register(WarehouseCategory)

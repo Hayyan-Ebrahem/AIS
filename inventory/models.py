3136b36625fe0a1b526ab0e7854dde4e7caa4a8d
khaldoonahmad <khaldoon_ahmad@hotmail.com>
2017-07-08 23:32:17 -0700

    corrections to run admin site

--- inventory/models.py
+++ inventory/models.py
@@ -9,6 +9,9 @@ class WarehouseCategory(models.Model):
 	user_id = models.TextField(max_length=15)
 	#should be related to user table will be done later
 
+	def __str__(self):
+		return self.name
+
 class Warehouse(models.Model):
 	code = models.TextField(primary_key=True, max_length=30)
 	name = models.TextField(max_length=30, unique=True)
@@ -20,3 +23,6 @@ class Warehouse(models.Model):
 	#should be related to user table will be done later
 	warehouse_keeper_id = models.TextField(max_length=15)
 	#should be related to user table will be done later
+
+	def __str__(self):
+		return self.name

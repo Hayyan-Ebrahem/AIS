6ef26ab13df5caf07bc3b638d001f13073f888c1
khaldoonahmad <khaldoon_ahmad@hotmail.com>
2017-07-08 17:49:33 -0700

    Add Customer and Cust.Category

--- customer/admin.py
+++ customer/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from customer.models import Customer
+from customer.models import CustomerCategory
 # Register your models here.
 
 admin.site.register(Customer)
+admin.site.register(CustomerCategory)

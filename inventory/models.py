c6a86834e5c1e910063f283bfdeeb759b725d20d
khaldoonahmad <khaldoon_ahmad@hotmail.com>
2017-07-10 21:18:46 -0700

    change the relation between wh. and cat to many to many

--- inventory/models.py
+++ inventory/models.py
@@ -2,12 +2,15 @@ from django.db import models
 from django.conf import settings
 # Create your models here.
 
-class WarehouseCategory(models.Model):
+class Category(models.Model):
 	code = models.TextField(primary_key=True, max_length=30)
 	name = models.TextField(max_length=30, unique=True)
 	created_at = models.DateField(auto_now_add=True)
 	user_id = models.TextField(max_length=15)
 	#should be related to user table will be done later
+
+	class metta:
+		verbose_name_plural = 'Categories'
 
 	def __str__(self):
 		return self.name
@@ -17,7 +20,7 @@ class Warehouse(models.Model):
 	name = models.TextField(max_length=30, unique=True)
 	location = models.TextField(max_length=50)
 	#chart_of_account_no = models.ForeignKey(ChartOfAccountNo, on_delete=models.CASCADE)
-	category = models.ForeignKey(WarehouseCategory, on_delete=models.CASCADE)
+	category_code = models.ManyToManyField(Category, through='WarehouseCategory')
 	created_at = models.DateField(auto_now_add=True)
 	user_id = models.TextField(max_length=15)
 	#should be related to user table will be done later
@@ -26,3 +29,10 @@ class Warehouse(models.Model):
 
 	def __str__(self):
 		return self.name
+
+class WarehouseCategory(models.Model):
+	warehouse_code = models.ForeignKey(Warehouse)
+	category_code = models.ForeignKey(Category)
+
+	def __str__(self):
+		return 'Category'

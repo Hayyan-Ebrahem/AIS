9fcf63bc4c0783a82da5c07be91284df2d0544d1
hayyan <hayyan-ebrahem@hotmail.com>
2017-07-29 09:10:48 +0200

    Product templates

--- product/views.py
+++ product/views.py
@@ -4,16 +4,18 @@ from .models import Product
 
 class ProductListView(ListView):
     model = Product
-    template_name = 'product_list.html'
-
-
+    template_name = 'product/product_list.html'
 
 
 
 class ProductDetailView(DetailView):
     model = Product
+    template_name = 'product/product_detail.html'
 
     def get_context_data(self, **kwargs):
         context = super(ProductDetailView, self).get_context_data(**kwargs)
-        context['product_details'] = Product.objects.filter(slug=self.kwargs['slug'])
+        context['product_details'] = self.object.categories.all()
+        for p in context['product_details']:
+        	print(p.name)
+
         return context
